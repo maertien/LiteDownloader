2e0b9548d1b4a5fb72fb9455ce1c273a9c2eee8f
Martin Kumst <martin.kumst@forpsi.com>
2017-12-20 14:20:09 +0100

    Data size limit added, getter for downloaded bytes count added

--- LiteDownloader.php
+++ LiteDownloader.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
  * Simple PHP downloader class
  * Author: Martin Kumst 
  * License: GNU GPL version 2
  */
-
 class LiteDownloader {
 
 	private $url;
@@ -12,19 +12,34 @@ class LiteDownloader {
 	private $agent;
 	private $contentType;
 	private $responseCode;
+	private $sizeLimit;
+	private $downloadedBytes;
 
 	/**
 	 * Make new downloader
 	 * @param string $url Url 
 	 * @param string $agent HTTP-USER-AGENT identification
+	 * @param mixed $sizeLimit Size limit of download
 	 */
-	public function __construct($url, $agent = "LiteDownloader/1") {
+	public function __construct($url, $agent = "LiteDownloader/1.1", $sizeLimit = null) {
 
 		$this->url = $url;
 		$this->content = null;
 		$this->agent = $agent;
 		$this->contentType = null;
 		$this->responseCode = null;
+		$this->sizeLimit = $sizeLimit;
+
+		$this->downloadedBytes = 0;
+	}
+
+	/**
+	 * Returns the size of downloaded data
+	 * @return int Downloaded bytes count
+ 	 */
+	public function getDownloadedBytes() {
+
+		return $this->downloadedBytes;
 	}
 
 	/**
@@ -47,11 +62,28 @@ class LiteDownloader {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // ;-)
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 300);
+		curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+		curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, array($this, "checkSize"));
+
 		$res = curl_exec($ch);
 
 		if ($res === false) {
+			
+			$message = "Unable to download content";
+
+			$curlErrorCode = curl_errno($ch);
+			if ($curlErrorCode !== 0) {
+
+				$message .= " - " . $curlErrorCode;
+			}
+
+			$curlErrorMessage = curl_error($ch);
+			if ($curlErrorMessage !== "") {
+
+				$message .= " - " . $curlErrorMessage;
+			}
 		
-			throw new Exception("Unable to download content", 3);
+			throw new Exception($message, 3);
 		}
 
 		$this->contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
@@ -87,5 +119,27 @@ class LiteDownloader {
 	public function getResponseCode() {
 
 		return $this->responseCode;
+	}
+
+	/**
+	 * Callback function for curl checking size of downloaded data
+	 */
+	private function checkSize($resource, $bytesExpected, $bytesDownloaded, $uploadExpected, $bytesUploaded) {
+
+		$this->downloadedBytes = $bytesDownloaded;
+
+		// Check if there is no limit set
+		if ($this->sizeLimit === null) {
+
+			return 0; // Download can continue
+		}
+
+		// Check size of downloaded data 
+		if ($bytesDownloaded > $this->sizeLimit) {
+
+			return -1; // Download stops now
+		}
+
+		return 0;
 	}
 }
--- test.php
+++ test.php
@@ -3,13 +3,17 @@
 require_once "./LiteDownloader.php";
 
 try {
-
+	// Test download without size limit
 	$ld = new LiteDownloader("http://idem.cz", "POKUS");
+
+	// Test download with size limit
+	// $ld = new LiteDownloader("https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-9.3.0-amd64-netinst.iso", "POKUS", 1024 * 1024 * 5);
 	$ld->download();
 
-	var_dump($ld->getContent());
-	var_dump($ld->getContentType());
+//	var_dump($ld->getContent());
+//	var_dump($ld->getContentType());
 	var_dump($ld->getResponseCode());
+	var_dump($ld->getDownloadedBytes());
 }
 catch (Exception $e) {
 
